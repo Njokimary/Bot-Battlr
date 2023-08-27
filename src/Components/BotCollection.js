4dc127359134ecc57c94c9b2b48a74f175f13b55
MARY NJOKI <marynjoki738@gmail.com>
2023-08-27 16:51:55 +0300

    create a function to enlist bots

--- src/Components/BotCollection.js
+++ src/Components/BotCollection.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import "./BotCollection.css";
 
-function BotCollection() {
+function BotCollection({ enlistBot, enlistedBots }) {
   const [bots, setBots] = useState([]);
 
   useEffect(() => {
@@ -15,7 +15,7 @@ function BotCollection() {
       <h2>Bot <span>Collection</span></h2>
       <div className="bot-card-container">
         {bots.map(bot => (
-          <div className="bot-card" key={bot.id}>
+          <div className={`bot-card ${enlistedBots.some(b => b.id === bot.id) ? 'enlisted' : ''}`} key={bot.id}>
             <img src={bot.avatar_url} alt={bot.name} />
             <h3>{bot.name}</h3>
             <p>Health: {bot.health}</p>
@@ -23,6 +23,11 @@ function BotCollection() {
             <p>Armor: {bot.armor}</p>
             <p>Class: {bot.bot_class}</p>
             {/* <p>Catchphrase: {bot.catchphrase}</p> */}
+            {enlistedBots.some(b => b.id === bot.id) ? (
+              <p className="enlisted-message">Enlisted</p>
+            ) : (
+              <button onClick={() => enlistBot(bot)}>Enlist</button>
+            )}
           </div>
         ))}
       </div>
