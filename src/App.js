03f735644d4b3367def34ddf392a9391db6ec6a6
MARY NJOKI <marynjoki738@gmail.com>
2023-08-27 17:10:32 +0300

    create a function enlistbot

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-
-import './App.css';
+import React, { useState } from 'react';
 import BotCollection from './Components/BotCollection';
+import YourBotArmy from './Components/YourBotArmy';
+import './App.css';
 
 function App() {
+  const [enlistedBots, setEnlistedBots] = useState([]);
+
+  const enlistBot = (bot) => {
+    if (!enlistedBots.some(b => b.id === bot.id)) {
+      setEnlistedBots([...enlistedBots, bot]);
+    }
+  };
+
   return (
-    <div className="App">
-      <BotCollection/>
+    <div className="app-container">
+    
+      <main>
+        <section className="bot-collection">
+          <BotCollection enlistBot={enlistBot} enlistedBots={enlistedBots} />
+        </section>
+        <section className="your-bot-army">
+          <YourBotArmy enlistedBots={enlistedBots} setEnlistedBots={setEnlistedBots} />
+        </section>
+      </main>
     </div>
   );
 }
