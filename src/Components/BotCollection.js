b135256ab637afde2ed7daaf3bf28bb4f3ae7ecb
MARY NJOKI <marynjoki738@gmail.com>
2023-08-27 12:29:25 +0300

    add classnames for styling

--- src/Components/BotCollection.js
+++ src/Components/BotCollection.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import "./BotCollection.css";
 
 function BotCollection() {
   const [bots, setBots] = useState([]);
@@ -10,19 +11,21 @@ function BotCollection() {
   }, []);
 
   return (
-    <div>
-      <h2>Bot Collection</h2>
-      {bots.map(bot => (
-        <div key={bot.id}>
-          <img src={bot.avatar_url} alt={bot.name} /> {/* Display image */}
-          <h3>{bot.name}</h3> {/* Display title */}
-          <p>Health: {bot.health}</p>
-          <p>Damage: {bot.damage}</p>
-          <p>Armor: {bot.armor}</p>
-          <p>Class: {bot.bot_class}</p>
-          <p>Catchphrase: {bot.catchphrase}</p>
-        </div>
-      ))}
+    <div className="container">
+      <h2>Bot <span>Collection</span></h2>
+      <div className="bot-card-container">
+        {bots.map(bot => (
+          <div className="bot-card" key={bot.id}>
+            <img src={bot.avatar_url} alt={bot.name} />
+            <h3>{bot.name}</h3>
+            <p>Health: {bot.health}</p>
+            <p>Damage: {bot.damage}</p>
+            <p>Armor: {bot.armor}</p>
+            <p>Class: {bot.bot_class}</p>
+            {/* <p>Catchphrase: {bot.catchphrase}</p> */}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
